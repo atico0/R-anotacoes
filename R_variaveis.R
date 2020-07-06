bebaf204481d4b515fe6c1600b66c3edb68825d6
atico0 <60025445+atico0@users.noreply.github.com>
2020-07-06 20:41:16 -0300

    adicionando novos arquivos de anotações

---
+++ R_variaveis.R
@@ -0,0 +1,19 @@
+#ATRIBUIÇÃO
+
+#No R as variavéis podem ser atribuidas tanto usando "=" quanto "<-"
+
+v1 = 54
+v2 = '88'
+
+#para saber quais VAR's estão na memoria você pode usar a função "ls()"
+ls()
+#Também é possivel saber o quais os dados contidos em cada VAR execuntando o nome dela
+v1
+
+#DELETANDO
+
+#Para deletar basta usar a função "rm(VAR)"
+rm(v1)
+#também é possivel combinar funções para apagar todas as variaveis 
+variaveis = ls()
+rm(variaveis)
