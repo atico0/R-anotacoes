e6a5a1af2d511c520729cfa5989c193b942e631c
atico0 <60025445+atico0@users.noreply.github.com>
2020-07-14 21:18:12 -0300

    agr foi

--- R_dataframes.R
+++ R_dataframes.R
@@ -22,9 +22,9 @@ str(resumo_df) #OBS: neste casos será necessário reatribuir kda coluna numerico 
 ## Opção 2
 #cria um dataframe com dados vetores ainda não criados
 dados = data.frame(fornecedores=c("A","B","E","D","X"),
-           quant_Jan=c(320,230,100,340,10),
-           quant_Fev=c(220,630,60,50,60),
-           quant_Mar=c(520,430,100,34,10))
+                   quant_Jan=c(320,230,100,340,10),
+                   quant_Fev=c(220,630,60,50,60),
+                   quant_Mar=c(520,430,100,34,10))
 #OBS: nesse caso é necessario explicitar os nomes das variaveis porque se não ela será nomeada como kda
 # elemento nela presente
 dados
@@ -54,6 +54,9 @@ resumo_df[2:4,] #acessando uma sequência de linhas
 resumo_df[c(1,5),] #acessando as linhas a partir de um vetor
 
 resumo_df$quant_Jan #acessando coluna $quant_Jan
+
+resumo_df[,'quant_Jan'] #acessando coluna $quant_Jan
+
 
 resumo_df[,2] #acessando coluna $quant_Jan pelo indice e não pelo nome (mesmo resultado do acesso acima)
 
@@ -112,6 +115,11 @@ resumo$quant_Fev[2:4]<-c(640,75,60)
 resumo$quant_Mar[c(1,4)]<-c(130,400)
 
 resumo$fornecedores<-c("Camil","Zaeli","Tio João","Danone","Liza")
+
+resumo[5,] <-c("Friboi",0,0,55,-3) 
+
+resumo[2,] <-c("Fuckboy",-2,4,556,-3) 
+
 # Eu acho essa parte não precisa de explicação
 
 # Adicionar variáveis ao Dataframe
@@ -124,15 +132,25 @@ resumo <- resumo[,-5] #remove 5 coluna
 
 # Adicionando e removendo casos
 
-
-resumo[6,] <-c("Friboi",0,0,55,-3) #adciniona esse vetor a linha 6
-
 resumo <- resumo[-6,] # retira linha 6
 
+resumo <- resumo[,-2] #retira coluna 2
 
 resumo <-resumo[-(c(1,4)),] # retira as linhas 1 e 4
 
-
+#para adicionar uma nova linhas é necessario criar uma matriz  com as linhas que se deseja adicionar
+linha1 = c('Z',999,223,123)
+linha2 = c('K',99,23,13)
+novas_linhas =  matrix(c(linha1,linha2),nrow = 2,byrow = T)
+# (AS LINHAS PRECISAM TER OS MESMOS NOMES DO DATAFRAME)
+nomes = c('fornecedores', 'quant_Jan', 'quant_Fev', 'quant_Mar')
+names(resumo) = nomes
+novas_linhas =  matrix(c(linha1,linha2),nrow = 2,ncol = 4,byrow = T,
+                       dimnames = as.list(c('linha1','linha2'), 
+                                          c('fornecedores', 'quant_Jan', 'quant_Fev', 'quant_Mar')))
+#criar uma segunda matriz (usando o rbind())
+#comoposta pela
+?matrix()
 
 ### OPERAÇÕES
 ncol(resumo)
@@ -162,11 +180,31 @@ mean(resumo$quant_Jan) # media
 median(resumo$quant_Jan) # mediana
 sd(resumo$quant_Jan) # desvio padrão
 
+##Vale ressaltar que:
+#É possivel ordenar o dataframe por uma coluna especifica
 
+fornecedores<-c("X","E","B","A","D")
+quant_Jan<-c(320,230,100,340,10)
+quant_Fev<-c(220,630,60,50,60)
+quant_Mar<-c(520,430,100,34,10)
 
+resumo<-cbind(fornecedores,quant_Jan,quant_Fev,quant_Mar)
 
+resumo
 
+resumo_df<-as.data.frame(resumo)
+resumo_df
+str(resumo_df) 
 
+order(resumo_df$fornecedores)# retorna um vetor com os indices da coluna fornecedores da forma que os
+#elementos dos seus respectivos itens 
+#ficariam se o dataframe fosse ordenado em função da coluna fornecedores
+
+resumo[order(resumo_df$fornecedores),]# Ordena o resumo em função da coluna fornecedores 
+#(NÃO afeta o dataframe original)
+
+resumo[order(resumo_df$fornecedores, decreasing = T),]# Ordena o resumo em função da coluna fornecedores 
+#(NÃO afeta o dataframe original) mas na em ordem decrescente 
 
 
 
