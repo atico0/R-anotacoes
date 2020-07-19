f1951c1a125b50aa1692b741932537c2c2e77fb3
atico0 <60025445+atico0@users.noreply.github.com>
2020-07-19 10:32:49 -0300

    adicionando funções

---
+++ R_funçoes.R
@@ -0,0 +1,103 @@
+
+###IF/ELSE/ELSE IF
+
+num = 99
+if (num%%2==0){
+  print('numero é par')
+  
+}else{
+  print('numero é impar')
+}
+
+
+num = 50
+
+if (num<50){
+  print('O NUMERO É MENOR QUE 50')
+  diferenca = 50-num
+  print('A DIFERENÇA É DE:')
+  print(diferenca)
+}else if(num>50){
+  print('O NUMERO É MAIOR QUE 50')
+  diferenca = num-50
+  print('A DIFERENÇA É DE')
+  print(diferenca)
+}else{
+  print('O NUMERO É IGUAL A 50')
+  
+}
+
+
+
+Vetor = 1:10
+
+#WHILE
+
+i = 1
+while(i<10){
+  print(i)
+  i = i+1
+}
+
+
+
+#FOR
+for (i in 1:10){
+  print(i)
+}
+
+
+
+#BREAK
+
+#sai de um laço
+
+
+#NEXT
+#ignora uma os comandos abaixo dele dentro de um laço sempre que ele for executado
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print()
+paste()
+cat()
+
+
