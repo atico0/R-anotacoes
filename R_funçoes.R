cd7397dccf4b9a41562c5bd3ad2eda0298277ce6
atico0 <60025445+atico0@users.noreply.github.com>
2020-07-24 17:23:18 -0300

    atualização
    a importação de arquivos não funcionoi

--- R_funçoes.R
+++ R_funçoes.R
@@ -48,12 +48,12 @@ for (i in 1:10){
 
 
 
-#BREAK
+##BREAK
 
 #sai de um laço
 
 
-#NEXT
+##NEXT
 #ignora uma os comandos abaixo dele dentro de um laço sempre que ele for executado
 
 
@@ -62,42 +62,113 @@ for (i in 1:10){
 
 
 
+##CRIANDO FUNÇÃO  
+#pra criar funções no R usa-se a função "function"
+function(parametros){#os parametros não são necessários
+  #ALGORITIMO DA FUNÇÃO
+  #""""""""""""""""""""  
+}
+#pra poder usar uma função é necessário atribui-la a uma VAR
+
+saudacao <- function(nome){
+  
+  print(paste('OLá',nome))
+}
+
+saudacao('mundo') #executa a função com o paramêtro "'mundo'"
+saudacao # dessa forma a função não é executada mas mostra como está escrita
+
+
+
+#também é possivel usar mais de um paramêtro nas funções
+xingamento = function(nome,xinga){
+  print(paste(nome,'é um',xinga))
+}
+
+xingamento('murilo','jogador de free fire')
+#note que a independete doque eu escreva oque importa é a ordem em que eu escrevo
+xingamento('gordo','edmilson')
+#mas é possivel consertar isso explicitando qual o parametro vem antes e depois
+xingamento(xinga = 'gordo',nome = 'edmilson')
+
+
+##Recriando as funções de tamanho sooma media variancia e desvio padrão
+#(length,sum,mean,var,sd) sem usar funções já previamente construidas
+
+tamanho <- function(vet) {
+  cont = 0
+  for (i in vet){
+    cont = cont + 1
+  }
+  return(cont)
+}
+length(1:10)
+tamanho(1:10)
+
+
+soma <- function(vet) {
+  cont = 0
+  for (i in vet)  {
+    cont = cont+i
+  }
+  return(cont)
+}
+sum(1:10)
+soma(1:10)
+
+media <- function(vet) {
+  return(soma(vet)/tamanho(vet))  
+}
+mean(1:10)
+media(1:10)
+
+
+
+variancia <- function(vet) {
+  v_media = media(vet)
+  v_tamanho = tamanho(vet)
+  i = 0
+  for (i in  1:v_tamanho){
+    vet[i] = (vet[i] - v_media) **2
+  }
+  return(soma(vet)/(v_tamanho-1))
+}
+var(1:10)
+variancia(1:10)
+
+
+desvio = function(vet){
+  return (sqrt(variancia(vet)))
+}
+sd(1:10)
+desvio(1:10)
+
+
+
+for (c in c(1:10)){
+  cat(c)
+  print('')
+}
+
+
+vet = c(1:10)
+for (i in 1:20){
+  print(paste(i))
+}
 
 
 
 
 
 
+print = function(){
+  cat('novo print')
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print()
 
 
 print()
 paste()
 cat()
-
 
---
+++ R_importando arquivos.R
@@ -0,0 +1,4 @@
+caminho  = file.path('C:','Users','core i3','Desktop','Pasta-de-trabalho.xlsx')
+read.xlsx('C:\Users\core i3\Documents\R')
+getwd()
+read.xlsx('C:\\Users\\core i3\\Documents\\R')
