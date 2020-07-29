993aef37da484684391833eefd24ef126f8e3dda
Luis Gabriel <60025445+atico0@users.noreply.github.com>
2020-07-29 20:36:29 -0300

    terminei

--- R_projeto.R
+++ R_projeto.R
@@ -1,15 +1,19 @@
+install.packages("forcats",dependencies = T)
+library("forcasts")
+
 alunos = function(alunos_dataframe,linha){
   linha_somada = linha
   resp = ''
-  while (resp != 'n') {
+  while (resp != 'n' ) {
     alunos_dataframe[linha_somada,1] = resposta <- readline(prompt = 'digite seu nome:')
-    alunos_dataframe[linha_somada,2] <- readline(prompt = "Digite seu sexo: ")
+    alunos_dataframe[linha_somada,2] <- (readline(prompt = "Digite seu sexo: [M/F]")
     for (c in 1:3){
       cat( "Digite sua ",c, ' nota')
       alunos_dataframe[linha_somada,2+c] = as.numeric(resposta <- readline(prompt =  ""))
     }
     alunos_dataframe[linha_somada,6] = round(mean(c(alunos_dataframe[linha_somada,3],
-                                     alunos_dataframe[linha_somada,4],alunos_dataframe[linha_somada,5])),2)
+                                     alunos_dataframe[linha_somada,4],
+                                     alunos_dataframe[linha_somada,5])),2)
     alunos_dataframe[linha_somada,7] = alunos_dataframe[linha,6]>=7
     
     cat('A média do aluno foi: ',alunos_dataframe[linha_somada,6])
@@ -32,8 +36,9 @@ relacao = data.frame(matrix(NA,ncol = 7))
 
 names(relacao) = c('NOME','SEXO','NOTA1','NOTA2','NOTA3','MEDIA','APROVADO')
 relacao[,c(3,4,5)] = as.numeric(relacao[,c(3,4,5)])
+relacao[,2] =(kelacao[,2])
 
-relacao = alunos(relacao,4)
+relacao = alunos(relacao,1)
 str(relacao)
 
 
