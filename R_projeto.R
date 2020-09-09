5573da9c353119272015576a4c3b608a3b5510c1
Luis Gabriel <60025445+atico0@users.noreply.github.com>
2020-09-09 12:15:19 -0300

    .

--- R_projeto.R
+++ R_projeto.R
@@ -6,7 +6,7 @@ alunos = function(alunos_dataframe,linha){
   resp = ''
   while (resp != 'n' ) {
     alunos_dataframe[linha_somada,1] = resposta <- readline(prompt = 'digite seu nome:')
-    alunos_dataframe[linha_somada,2] <- (readline(prompt = "Digite seu sexo: [M/F]")
+    alunos_dataframe[linha_somada,2] <- readline(prompt = "Digite seu sexo: [M/F]")
     for (c in 1:3){
       cat( "Digite sua ",c, ' nota')
       alunos_dataframe[linha_somada,2+c] = as.numeric(resposta <- readline(prompt =  ""))
@@ -14,7 +14,7 @@ alunos = function(alunos_dataframe,linha){
     alunos_dataframe[linha_somada,6] = round(mean(c(alunos_dataframe[linha_somada,3],
                                      alunos_dataframe[linha_somada,4],
                                      alunos_dataframe[linha_somada,5])),2)
-    alunos_dataframe[linha_somada,7] = alunos_dataframe[linha,6]>=7
+    alunos_dataframe[linha_somada,7] = alunos_dataframe[linha_somada,6]>=7
     
     cat('A média do aluno foi: ',alunos_dataframe[linha_somada,6])
     print('')
@@ -23,9 +23,10 @@ alunos = function(alunos_dataframe,linha){
     }else{
       print('ALUNO REPROVAOD')
     }
-    linha_somada = linha_somada+1
+    linha_somada = linha_somada + 1
     resp =  readline(prompt = 'Quer continuar? [S/N]')
   }
+  alunos_dataframe
   return(alunos_dataframe)
 }
 
@@ -36,10 +37,8 @@ relacao = data.frame(matrix(NA,ncol = 7))
 
 names(relacao) = c('NOME','SEXO','NOTA1','NOTA2','NOTA3','MEDIA','APROVADO')
 relacao[,c(3,4,5)] = as.numeric(relacao[,c(3,4,5)])
-relacao[,2] =(kelacao[,2])
 
 relacao = alunos(relacao,1)
-str(relacao)
 
 
 relacao[3,] = 'a'
@@ -49,6 +48,5 @@ relacao[3,] = 'a'
 c(1:10,'oi')
 
 
-
-
+8>=7
 
